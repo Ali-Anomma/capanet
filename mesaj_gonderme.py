6337f03d6f402bbba3870ed49b91dfa8ac7dcd94
tunahanaktas <92438644+tunahanaktas@users.noreply.github.com>
2021-10-13 14:35:10 +0300

    Create mesaj_gonderme.py

---
+++ mesaj_gonderme.py
@@ -0,0 +1,62 @@
+while True:
+    message = input('Me : ')
+    conn.send(message.encode())
+    message = conn.recv(1024)
+    message = message.decode()
+    print(client, ':', message)
+    socket_server.send(name.encode())
+server_name = socket_server.recv(1024)
+server_name = server_name.decode()
+ 
+print(server_name,' has joined...')
+while True:
+    message = (socket_server.recv(1024)).decode()
+    print(server_name, ":", message)
+    message = input("Me : ")
+    socket_server.send(message.encode()) 
+    def listen_for_messages():
+    while True:
+        message = s.recv(1024).decode()
+        print("\n" + message)
+
+# make a thread that listens for messages to this client & print them
+t = Thread(target=listen_for_messages)
+# make the thread daemon so it ends whenever the main thread ends
+t.daemon = True
+# start the thread
+t.start()
+while True:
+    # input message we want to send to the server
+    to_send =  input()
+    # a way to exit the program
+    if to_send.lower() == 'q':
+        break
+    # add the datetime, name & the color of the sender
+    date_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S') 
+    to_send = f"{client_color}[{date_now}] {name}{separator_token}{to_send}{Fore.RESET}"
+    # finally, send the message
+    s.send(to_send.encode())
+
+# close the socket
+s.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
